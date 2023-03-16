da28932c0112e5606f93176048db837e9199bd78
Henry Kondratko <henry.kondratko@gmail.com>
2023-03-15 21:10:05 -0400

    updated page title and added test for page render

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const pageTitle = screen.getByRole('heading', {name: /give me the recipe/i})
+  expect(pageTitle).toBeInTheDocument();
 });
