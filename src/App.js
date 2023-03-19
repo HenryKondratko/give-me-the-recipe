7fd25129a70b5cf2c7bdf8e7940c2e45002f743c
Henry Kondratko <henry.kondratko@gmail.com>
2023-03-19 15:10:05 -0400

    extracted InputBox into component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import InputBox from "./components/InputBox";
 
 function App() {
     return (
@@ -8,8 +9,7 @@ function App() {
                 <h1>Give Me The Recipe</h1>
             </header>
             <body>
-            <label htmlFor="url">Enter URL and hit Submit</label>
-            <input type="text" id="url"/>
+            <InputBox/>
             </body>
         </div>
     );
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InputBox = () => {
+    return (
+        <div>
+            <label htmlFor="url">Enter URL and hit Submit</label>
+            <input type="text" id="url"/>
+        </div>
+    )
+}
+
+export default InputBox;
